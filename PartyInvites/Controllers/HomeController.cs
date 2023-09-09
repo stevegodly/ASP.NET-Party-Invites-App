a9aa1406231839f9679eeab3c34891f65848ddd1
stevegodly <stevegodly12@gmail.com>
2023-09-09 12:11:32 +0530

    1st commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Diagnostics;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour=DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon" : "Good Morning";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+        [HttpGet]
+        public IActionResult RSVP()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RSVP(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guest);
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(".+\\@.+\\..+")]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public bool? WillAttend { get; set; } 
+    }
+}
